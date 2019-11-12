ef6217324180fd11ffbd46f4273ba811deb3ee9c
Maddy Long <madeline.long.qr@gmail.com>
2019-11-12 14:33:05 +1100

    Completed to-do without styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all
+
+  # new has to go before show
+  delete 'tasks/:id', to: 'tasks#destroy'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
---
+++ db/migrate/20191111231516_generated_model_and.rb
@@ -0,0 +1,4 @@
+class GeneratedModelAnd < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
